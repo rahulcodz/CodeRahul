dc60fdc27e9bb773b6f53b7db9adabad7afe2c96
Rahul Panchal <127726141+rahulcodz@users.noreply.github.com>
2024-07-11 06:01:01 +0000

    node js linter formatter

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
-import express from "express";
+import express from 'express'
+import 'dotenv/config'
 
-const app = express();
+const PORT = process.env.PORT || 8000
+
+const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello, Node.js with nodemon!');
-});
+    res.send('Hello, Node.js with nodemon!')
+})
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+app.listen(PORT, () => {
+    console.log('Server is running on http://localhost:' + PORT)
+})
