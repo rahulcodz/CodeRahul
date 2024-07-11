3672ecf3f810e315db83645f9ea028db21e5087e
Rahul Panchal <127726141+rahulcodz@users.noreply.github.com>
2024-07-11 06:07:11 +0000

    misc

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-import express from 'express'
-import 'dotenv/config'
+import express from 'express';
+import 'dotenv/config';
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, Node.js with nodemon!')
-})
+    res.send('Hello, Node.js with nodemon!');
+});
 
 app.listen(PORT, () => {
-    console.log('Server is running on http://localhost:' + PORT)
-})
+    console.log('Server is running on http://localhost:' + PORT);
+});
