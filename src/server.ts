f9468e59d513bc4c0c4352d0f8699128547a5f41
Rahul Panchal <127726141+rahulcodz@users.noreply.github.com>
2024-07-11 06:07:54 +0000

    misc

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
-    console.log('Server started')
-})
+    console.log('Server started');
+});
